1f05ca7aeb00192d6c2128ac733fee9fda3cc354
chorom-ham <chorom.m.ham@gmail.com>
2020-07-15 17:06:42 +0900

    Add hot tag component

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,22 @@ import Head from "next/head";
 import Gnb from "../src/components/organisms/Gnb";
 import Banner from "../src/components/organisms/Banner";
 import Preview from "../src/components/templates/Preview";
+import HotTag from "../src/components/organisms/HotTag";
+import styled from "styled-components";
 
 export default function Home() {
   return (
-    <>
+    <Wrapper>
       <Gnb></Gnb>
       <Banner></Banner>
       <Preview type="new"></Preview>
       <Preview type="most"></Preview>
-    </>
+      <HotTag></HotTag>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100%;
+`;
