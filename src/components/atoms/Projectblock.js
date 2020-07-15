619703900c0e9374b3a3e1bc490487fb6dfd6588
soeun lee <thdmsdl87@gmail.com>
2020-07-15 17:47:46 +0900

    Add Projectblock Wrapper

---
+++ src/components/atoms/Projectblock.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export default styled.div`
+  width: 23.6rem;
+  height: 12.8rem;
+  border-radius: 0.2rem;
+  box-shadow: 0 0.1rem 0.2rem 0 rgba(0, 0, 0, 0.1);
+  background-color: #ffffff;
+  position: relative;
+  box-sizing: border-box;
+`;
