fdf526601012b96f24ca5a2d3b51bcdc8b972384
soeun lee <thdmsdl87@gmail.com>
2020-07-17 18:28:44 +0900

    Add Input search

---
+++ src/components/molecules/Input/Search.js
@@ -0,0 +1,16 @@
+import Wrapper from "../../atoms/Input/Search"
+import Text from "../../atoms/Text"
+import Icon from "../../atoms/Icon/Search"
+import styled from "styled-components"
+
+export default function Search(){
+    return(
+        <Wrapper>
+            <Icon style={{width: "0.7rem", height: "0.7rem"}} fill="#8b90a0"></Icon>
+            &nbsp;
+                <Text level={2} color="#d3d4d8" line="0.9rem" width="7.4rem" align="left">
+                    검색
+                </Text>
+        </Wrapper>
+    );
+}
