d7bdb661439cdc628e7c7dcf201a5d6b50b91dec
soeun lee <thdmsdl87@gmail.com>
2020-07-21 18:57:05 +0900

    Change Icon margin

--- src/components/molecules/Input/Search.js
+++ src/components/molecules/Input/Search.js
@@ -6,8 +6,7 @@ import styled from "styled-components"
 export default function Search(){
     return(
         <Wrapper>
-            <Icon style={{width: "0.7rem", height: "0.7rem"}} fill="#8b90a0"></Icon>
-            &nbsp;
+            <Icon style={{width: "0.7rem", height: "0.7rem", margin: "0 0.52rem 0 0"}} fill="#8b90a0"></Icon>
                 <Text level={2} color="#d3d4d8" line="0.9rem" width="7.4rem" align="left">
                     검색
                 </Text>
