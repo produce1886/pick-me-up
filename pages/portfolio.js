7eef47b29b65076a2d3b6285c652d173462f3cca
soeun lee <thdmsdl87@gmail.com>
2020-07-21 23:48:10 +0900

    Add Filter and Gnb

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,3 +1,32 @@
+import styled from "styled-components";
+import Gnb from "../src/components/organisms/Gnb";
+import FilterSearch from "../src/components/organisms/FilterSearch"
 export default function Portfolio() {
-  return <></>;
+  return (
+    <>
+    <Gnb></Gnb>
+    <FilterWrapper>
+     <FilterSearch type="portfolio"></FilterSearch>
+   </FilterWrapper>
+    <Wrapper></Wrapper>
+  </>
+  );
 }
+
+const FilterWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 0.01rem solid #d3d4d8;
+`;
+
+const Wrapper = styled.div`
+  background-color: #f5edff;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+`;
