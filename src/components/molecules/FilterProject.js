76b2eba4a430d69db4de113ca5d22d71606e07fe
soeun lee <thdmsdl87@gmail.com>
2020-07-21 18:56:32 +0900

    Refactor FilterProject using props

--- src/components/molecules/FilterProject.js
+++ src/components/molecules/FilterProject.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
-import Filter from "../molecules/Filter/Off"
+import Filter from "../molecules/Filter"
 
 export default function FilterProject(){
     return (
         <Wrapper>
-            <Filter word="카테고리"></Filter>
-            <Filter word="구인분야"></Filter>
-            <Filter word="지역"></Filter>
-            <Filter word="프로젝트 종류"></Filter>
+            <Filter type="smalloff" word="카테고리"></Filter>
+            <Filter type="smalloff" word="구인분야"></Filter>
+            <Filter type="smalloff" word="지역"></Filter>
+            <Filter type="smalloff" word="프로젝트 종류"></Filter>
         </Wrapper>
     );
 }
@@ -17,8 +17,6 @@ const Wrapper = styled.div`
     height: 1.6rem;
     justify-content: space-between;
     align-items: center;
-    box-sizing: border-box;
-    bottom: 0;
     display: flex;
     flex-direction: row;
 `;
