0498f106ab58f58f69adef03d8ed981d84d7796b
soeun lee <thdmsdl87@gmail.com>
2020-07-21 18:57:46 +0900

    Add Project page

--- pages/project.js
+++ pages/project.js
@@ -1,11 +1,70 @@
-
+import styled from "styled-components";
 import Gnb from "../src/components/organisms/Gnb";
-
-
+import Projectlist from "../src/components/templates/Projectlist"
+import Filter from "../src/components/molecules/Filter"
+import FilterSearchProject from "../src/components/organisms/FilterSearchProject"
+import WriteIcon from "../src/components/atoms/Icon/Write"
 export default function Project() {
   return (
   <>
-  <Gnb></Gnb></>
+  <Gnb></Gnb>
+  <FilterWrapper>
+    <FilterSearchProject></FilterSearchProject>
+  </FilterWrapper>
+  <Wrapper>
+    <FilterListWrapper>
+      <Div>
+        <Filter type="bigoff" word="최신순"></Filter>
+      </Div>
+      <Projectlist></Projectlist>
+    </FilterListWrapper>
+    <IconWrapper>
+      <WriteIcon style={{width: "2.4rem", height: "2.4rem"}}></WriteIcon>
+    </IconWrapper>
+  </Wrapper>
+  </>
   
   );
 }
+
+const Wrapper = styled.div`
+  background-color: #f0f8fd;
+  justify-content: center;
+  width: 100%;
+  padding: 0.2rem 1rem 0.5rem 1rem;
+  box-sizing: border-box;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Div = styled.div`
+  width: 48rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin: 1.2rem 0 1.2rem 0;
+`;
+
+const FilterWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 0.01rem solid #d3d4d8;
+`;
+
+const FilterListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const IconWrapper = styled.div`
+  padding: ;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 13rem 2rem;
+`;
