148e44c901aff1d57e16db0947a0b61e0165fe6f
soeun lee <thdmsdl87@gmail.com>
2020-07-24 03:22:26 +0900

    Refactor Top Button background by using props

--- src/components/molecules/Button/Top.js
+++ src/components/molecules/Button/Top.js
@@ -1,30 +1,28 @@
 import Icon from "../../atoms/Icon/Arrow/up";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import Link from "next/link";
 import Text from "../../atoms/Text";
 
 export default function Button(props){
     return (
-        <Link href={props.link}>
-        <A>
-            <Wrapper>
+            <Wrapper background={props.background}>
                 <Icon style={{width: "1rem", height: "1rem", margin: "0.2rem 0 0 0"}} fill="#232735"></Icon>
                 <Text line="1rem" level={2} weight="20rem" align="center" color="#232735" deco="underline">TOP</Text>
             </Wrapper>
-        </A>
-        </Link>
     );
 }
 const A = styled.a``;
 
 const Wrapper=styled.button`
+${(props) => css`
     width: 1.3rem;
     justify-content: center;
     align-items: center;
     bottom: 0;
     display: flex;
     flex-direction: column;
-    background-color: #ffffff;
+    background-color: ${props.background};
     border: none;
-    padding: 0 23rem 0 0;
+    padding: 0 24rem 0 0;
+    `}
 `;
