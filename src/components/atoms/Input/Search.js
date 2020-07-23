d7da5f739fa642fe7224fac3c55c24bd1be712e3
chorom-ham <chorom.m.ham@gmail.com>
2020-07-23 17:26:15 +0900

    Fix search bar as input

--- src/components/atoms/Input/Search.js
+++ src/components/atoms/Input/Search.js
@@ -5,8 +5,10 @@ export default styled.div`
   height: 1.6rem;
   border-radius: 2rem;
   background-color: #f0f1f3;
-  display: flex;
-  justify-content: center;
   align-items: center;
   border: 0.04rem solid #d3d4d8;
+  padding: 0.4rem 0.8rem 0.4rem 0.8rem;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
 `;
