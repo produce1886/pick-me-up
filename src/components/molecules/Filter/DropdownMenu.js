95321cec38dec224e419f7b64c21700df446b0fd
soeun lee <thdmsdl87@gmail.com>
2020-07-31 16:03:40 +0900

    Fix id to key and Fix indentation

--- src/components/molecules/Filter/DropdownMenu.js
+++ src/components/molecules/Filter/DropdownMenu.js
@@ -6,29 +6,27 @@ import Text from "../../atoms/Text";
 import { useState } from "react";
 
 export default function DropdownMenu(props) {
-	
-		return (
-			<Wrapper zIndex={props.activeMenu==="align" ? "102" : "200"}>
-				<Hover
-					width="6.2rem"
-					height="1.8rem"
-					border="0rem"
-					borderColor="#ffffff"
-					backgroundColor="#ffffff">
-					<Text line="1.08rem" level={3} color="#9c69e2" align="left">
-							{props.activeMenu==="align" ? "최신순" : "전체보기"} 
-					</Text>
-				</Hover>
-				{props.data.map((value) => (
-					<ItemWrapper
-						link={value.link}
-						text={value.title}
-						selected={value.selected}
-						type={value.type}
-						id={value.id}
-						toggleSelected={props.toggleSelected}
-						></ItemWrapper>
-				))}
-			</Wrapper>
-		);
+	return (
+		<Wrapper zIndex={props.activeMenu === "align" ? "102" : "200"}>
+			<Hover
+				width="6.2rem"
+				height="1.8rem"
+				border="0rem"
+				borderColor="#ffffff"
+				backgroundColor="#ffffff">
+				<Text line="1.08rem" level={3} color="#9c69e2" align="left">
+					{props.activeMenu === "align" ? "최신순" : "전체보기"}
+				</Text>
+			</Hover>
+			{props.data.map((value) => (
+				<ItemWrapper
+					link={value.link}
+					text={value.title}
+					selected={value.selected}
+					type={value.type}
+					key={value.key}
+					toggleSelected={props.toggleSelected}></ItemWrapper>
+			))}
+		</Wrapper>
+	);
 }
