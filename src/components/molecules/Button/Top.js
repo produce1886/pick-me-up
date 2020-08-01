9182ac4f3a9f42c0db2ece74d782de5f6a8eb68f
chorom-ham <chorom.m.ham@gmail.com>
2020-08-01 22:58:16 +0900

    Add top button feature

--- src/components/molecules/Button/Top.js
+++ src/components/molecules/Button/Top.js
@@ -5,23 +5,27 @@ import Text from "../../atoms/Text";
 export default function TopButton() {
   return (
     <Button>
-      <Icon
-        style={{ width: "1rem", height: "1rem", margin: "0.2rem 0 0 0" }}
-        fill="#232735"
-      ></Icon>
-      <Text
-        line="1rem"
-        level={2}
-        weight="20rem"
-        align="center"
-        color="#232735"
-        deco="underline"
-      >
-        TOP
-      </Text>
+      <A href="#">
+        <Icon
+          style={{ width: "1rem", height: "1rem", margin: "0.2rem 0 0 0" }}
+          fill="#232735"
+        ></Icon>
+        <Text
+          line="1rem"
+          level={2}
+          weight="20rem"
+          align="center"
+          color="#232735"
+          deco="underline"
+        >
+          TOP
+        </Text>
+      </A>
     </Button>
   );
 }
+
+const A = styled.a``;
 
 const Button = styled.button`
   width: fit-content;
