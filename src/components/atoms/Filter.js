154584b204a036c1bea7d1e31b0485097edccece
chorom-ham <chorom.m.ham@gmail.com>
2020-08-01 20:10:18 +0900

    Fix style of filter

--- src/components/atoms/Filter.js
+++ src/components/atoms/Filter.js
@@ -1,19 +1,20 @@
 import styled, { css } from "styled-components";
 
 export default styled.div`
-	${(props) => css`
-		width: ${props.width};
-		height: ${props.height};
-		font-family: inherit;
-		border: ${props.border} solid ${props.borderColor};
-		background-color: ${props.backgroundColor};
-		border-radius: 0.2rem;
-		padding: 0 0.4rem 0 0.4rem;
-		margin: 0 0.8rem 0 0;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		align-items: center;
-		position: relative;
-	`}
+  ${(props) => css`
+    width: ${props.width};
+    height: ${props.height};
+    font-family: inherit;
+    border: ${props.border} solid ${props.borderColor};
+    background-color: ${props.backgroundColor};
+    border-radius: 0.2rem;
+    padding: 0 0.4rem 0 0.4rem;
+    margin: 0 0.8rem 0 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    box-sizing: border-box;
+  `}
 `;
