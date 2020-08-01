d3794d0d7b36c6817941f69bf31eda9133c17352
chorom-ham <chorom.m.ham@gmail.com>
2020-08-01 22:52:52 +0900

    Fix style of bottom buttons

--- src/components/atoms/BottomButtons.js
+++ src/components/atoms/BottomButtons.js
@@ -1,14 +1,14 @@
 import styled, { css } from "styled-components";
 
 export default styled.div`
-${(props) => css`
-  background-color: ${props.background};
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  box-sizing: border-box;
-  display: flex;
-  padding:0 0 2rem 0;
+  ${(props) => css`
+    background-color: ${props.background};
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    box-sizing: border-box;
+    display: flex;
+    padding: 0 0 2rem 0;
   `}
 `;
