69b2963f37863eeaa7616b53563d12871d89df15
chorom-ham <chorom.m.ham@gmail.com>
2020-08-10 18:14:02 +0900

    Add detail on portfolio block

--- src/components/organisms/Portfolioblock.js
+++ src/components/organisms/Portfolioblock.js
@@ -1,6 +1,29 @@
 import styled, { css } from "styled-components";
 import Wrapper from "../atoms/PortfolioBlock";
+import { Motion, spring } from "react-motion";
+import { useState } from "react";
 
 export default function PortfolioBlock() {
-  return <Wrapper></Wrapper>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <Wrapper
+      onMouseOver={() => setShow(true)}
+      onMouseOut={() => setShow(false)}
+    >
+      <DetailWrapper y={show ? "-5rem" : "5rem"}></DetailWrapper>
+    </Wrapper>
+  );
 }
+
+const DetailWrapper = styled.div`
+  width: 100%;
+  height: 5rem;
+  position: absolute;
+  bottom: -5rem;
+  background-color: #888;
+  ${(props) => css`
+    webkit-transform: translateY(${props.y});
+    transform: translateY(${props.y});
+  `}
+`;
