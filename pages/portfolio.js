6b0665df56c41c34c26d427444cb0117e169925a
soeun lee <thdmsdl87@gmail.com>
2020-08-12 23:33:08 +0900

    Add Modal to page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import Gnb from "../src/components/organisms/Gnb";
 import FilterSearch from "../src/components/organisms/FilterSearch"
 import BottomButtons from "../src/components/organisms/BottomButtons";
-import PortfolioBody from "../src/components/templates/PortfolioBody"
+import PortfolioBody from "../src/components/templates/PortfolioBody";
+import ModalBlock from "../src/components/organisms/Modalblock";
 export default function Portfolio() {
 
   return (
@@ -10,6 +11,7 @@ export default function Portfolio() {
     <Gnb></Gnb>
     <FilterSearch type="portfolio"></FilterSearch>
     <PortfolioBody></PortfolioBody>
+    <ModalBlock></ModalBlock>
     <BottomButtons background="#f5edff"></BottomButtons>
   </>
   );
--- pages/project.js
+++ pages/project.js
@@ -3,7 +3,7 @@ import Gnb from "../src/components/organisms/Gnb";
 import ProjectBody from "../src/components/templates/ProjectBody"
 import BottomButtons from "../src/components/organisms/BottomButtons";
 import FilterSearch from "../src/components/organisms/FilterSearch";
-
+import ModalBlock from "../src/components/organisms/Modalblock"
 export default function Project(props) {
   
   return (
@@ -11,6 +11,7 @@ export default function Project(props) {
       <Gnb></Gnb>
       <FilterSearch type="project"></FilterSearch>
       <ProjectBody></ProjectBody>
+      <ModalBlock type="project"></ModalBlock>
       <BottomButtons background="#f0f8fd"></BottomButtons>
     </>
   );
