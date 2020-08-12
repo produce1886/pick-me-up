2bd9fc2e327811b6832c9f523252f89700135ca9
soeun lee <thdmsdl87@gmail.com>
2020-08-13 02:49:51 +0900

    Remove Modalblock

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,18 +1,16 @@
 import styled from "styled-components";
 import Gnb from "../src/components/organisms/Gnb";
-import FilterSearch from "../src/components/organisms/FilterSearch"
+import FilterSearch from "../src/components/organisms/FilterSearch";
 import BottomButtons from "../src/components/organisms/BottomButtons";
 import PortfolioBody from "../src/components/templates/PortfolioBody";
 import ModalBlock from "../src/components/organisms/Modalblock";
 export default function Portfolio() {
-
-  return (
-    <>
-    <Gnb></Gnb>
-    <FilterSearch type="portfolio"></FilterSearch>
-    <PortfolioBody></PortfolioBody>
-    <ModalBlock></ModalBlock>
-    <BottomButtons background="#f5edff"></BottomButtons>
-  </>
-  );
+	return (
+		<>
+			<Gnb></Gnb>
+			<FilterSearch type="portfolio"></FilterSearch>
+			<PortfolioBody></PortfolioBody>
+			<BottomButtons background="#f5edff"></BottomButtons>
+		</>
+	);
 }
--- pages/project.js
+++ pages/project.js
@@ -1,20 +1,17 @@
 import styled from "styled-components";
 import Gnb from "../src/components/organisms/Gnb";
-import ProjectBody from "../src/components/templates/ProjectBody"
+import ProjectBody from "../src/components/templates/ProjectBody";
 import BottomButtons from "../src/components/organisms/BottomButtons";
 import FilterSearch from "../src/components/organisms/FilterSearch";
-import ModalBlock from "../src/components/organisms/Modalblock"
+import ModalBlock from "../src/components/organisms/Modalblock";
+import Modal, { ModalProvider } from "react-modal-hook";
 export default function Project(props) {
-  
-  return (
-    <>
-      <Gnb></Gnb>
-      <FilterSearch type="project"></FilterSearch>
-      <ProjectBody></ProjectBody>
-      <ModalBlock type="project"></ModalBlock>
-      <BottomButtons background="#f0f8fd"></BottomButtons>
-    </>
-  );
+	return (
+		<>
+			<Gnb></Gnb>
+			<FilterSearch type="project"></FilterSearch>
+			<ProjectBody></ProjectBody>
+			<BottomButtons background="#f0f8fd"></BottomButtons>
+		</>
+	);
 }
-
-
