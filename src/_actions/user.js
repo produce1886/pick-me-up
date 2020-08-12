beea8d1b176fc3ea4b9345bf447dd2c546f377bb
chorom-ham <chorom.m.ham@gmail.com>
2020-08-13 00:03:35 +0900

    Implement persist login after refresh

--- src/_actions/user.js
+++ src/_actions/user.js
@@ -1,6 +1,8 @@
 import { LOGIN_USER, LOGOUT_USER } from "./types";
+import Cookies from "js-cookie";
 
 export function loginUser(data) {
+  Cookies.set("userData", data, { expires: 0.1 });
   return {
     type: LOGIN_USER,
     payload: data,
--- src/_reducers/user.js
+++ src/_reducers/user.js
@@ -1,18 +1,29 @@
 import { LOGIN_USER, LOGOUT_USER } from "../_actions/types";
+import Cookies from "js-cookie";
 
-export const initialState = {
-  isSignedIn: false,
-  userData: {
-    name: "",
-    email: "",
-    profile_pic: "",
-  },
-};
+let userData = Cookies.get("userData");
+
+let initialState;
+if (userData) {
+  initialState = {
+    isSignedIn: true,
+    userData: JSON.parse(userData),
+  };
+} else {
+  initialState = {
+    isSignedIn: false,
+    userData: {
+      name: "",
+      email: "",
+      profilePic: "",
+    },
+  };
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_USER: {
-      return { isSignedIn: true, userData: action.payload };
+      return { ...state, isSignedIn: true, userData: action.payload };
     }
     case LOGOUT_USER: {
       return initialState;
