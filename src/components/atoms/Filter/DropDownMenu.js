c142831bb39b5ba097a9ee9338738ea4f60a3223
soeun lee <thdmsdl87@gmail.com>
2020-08-12 15:42:01 +0900

    Fix width and box-shadow

--- src/components/atoms/Filter/DropDownMenu.js
+++ src/components/atoms/Filter/DropDownMenu.js
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 
 export default styled.div`
 	${(props) => css`
-		width: 6.84rem;
+		width: 5.4rem;
 		height: fit-content;
 		border: none;
 		background-color: #ffffff;
@@ -13,8 +13,9 @@ export default styled.div`
 		flex: none;
 		position: absolute;
 		top: 1.7rem;
-		left:-0.2rem;
-		box-shadow: 0.1rem 0.1rem 0.2rem 0 rgba(0, 0, 0, 0.1);
+		left: -0.1rem;
+		box-shadow: 0rem 0.2rem 0.5rem rgba(0, 0, 0, 0.1), 
+		0.7rem 0 0.5rem rgba(0, 0, 0, 0.1);
 		z-index: ${props.zIndex};
 	`}
 `;
