84c50445cbe8e8b542db8d55c23e764795b1a537
chorom-ham <chorom.m.ham@gmail.com>
2020-08-15 15:27:26 +0900

    Fox style

--- src/components/templates/Projectlist.js
+++ src/components/templates/Projectlist.js
@@ -1,49 +1,47 @@
 import styled from "styled-components";
-import Projectblock from "../organisms/ProjectBlock";
+import ProjectBlock from "../organisms/ProjectBlock";
 
 export default function Projectlist() {
   return (
     <Wrapper>
       <Div>
-        <Projectblock></Projectblock>
-        <Projectblock></Projectblock>
+        <ProjectBlock></ProjectBlock>
+        <ProjectBlock></ProjectBlock>
       </Div>
       <Div>
-        <Projectblock></Projectblock>
-        <Projectblock></Projectblock>
+        <ProjectBlock></ProjectBlock>
+        <ProjectBlock></ProjectBlock>
       </Div>
       <Div>
-        <Projectblock></Projectblock>
-        <Projectblock></Projectblock>
+        <ProjectBlock></ProjectBlock>
+        <ProjectBlock></ProjectBlock>
       </Div>
       <Div>
-        <Projectblock></Projectblock>
-        <Projectblock></Projectblock>
+        <ProjectBlock></ProjectBlock>
+        <ProjectBlock></ProjectBlock>
       </Div>
       <Div>
-        <Projectblock></Projectblock>
-        <Projectblock></Projectblock>
+        <ProjectBlock></ProjectBlock>
       </Div>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  width: 48.5rem;
-  height: 67.68rem;
+  width: 100%;
+  height: fit-content;
   justify-content: center;
   align-items: center;
   box-sizing: border-box;
-  bottom: 0;
   display: flex;
   flex-direction: column;
   margin: 0 0 2rem 0;
 `;
 
 const Div = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
-  margin: 0.45rem 0 0.45rem 0;
+  justify-content: space-between;
 `;
