25f678fe6f4e256011461268615d2da49ba882a7
chorom-ham <chorom.m.ham@gmail.com>
2020-08-16 21:34:14 +0900

    Fix dropzone errors

--- src/components/molecules/Modal/Dropzone.js
+++ src/components/molecules/Modal/Dropzone.js
@@ -1,17 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
-import { useState, useEffect } from "react";
 import Text from "../../atoms/Text";
 import Upload from "../../atoms/Icon/Upload";
 import Close from "../../atoms/Icon/Close";
-export default function Dropzone() {
-  const [selectedFiles, setSelectFile] = useState([]); //전체 file들
-  const [imagePreviewUrl, setImagePreviewUrl] = useState([]);
 
-  const removeFile = (data) => {
-    const newArray = selectedFiles.filter((file) => file.data !== data);
-    setSelectFile([newArray]);
-  }; // x 누를 시 선택된 파일 제거
+export default function Dropzone() {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [preview, setPreview] = useState([]);
+
+  const removeFile = (file) => {
+    const currentIndex = selectedFiles.indexOf(file);
+    let newArray = [...selectedFiles];
+    newArray.splice(currentIndex, 1);
+    setSelectedFiles(newArray);
+    setPreview(newArray);
+  };
 
   const dragOver = (e) => {
     e.preventDefault();
@@ -28,10 +31,13 @@ export default function Dropzone() {
   const fileDrop = (e) => {
     e.preventDefault();
     const files = e.dataTransfer.files;
-    if (files.length) {
-      handleFiles(files);
+    if (files.length > 0) {
+      for (let i = 0; i < files.length; i++) {
+        handleFile(files[i]);
+      }
     }
   };
+
   const validateFile = (file) => {
     const validTypes = [
       "image/jpeg",
@@ -44,29 +50,50 @@ export default function Dropzone() {
       return false;
     }
     return true;
-  }; //file 형식 검증
-  const imagePreview = (files) => {
-    let reader = new FileReader();
-    reader.onloadend = () => {
-      setImagePreviewUrl((prevArray) => [...prevArray, reader.result]);
-      //add to an array so we can display the name of file
-      files["invalid"] = false; // add a new property called invalid
-      files["data"] = reader.result;
-    };
-    reader.readAsDataURL(files);
   };
 
-  const handleFiles = (files) => {
-    for (let i = 0; i < files.length; i++) {
-      if (validateFile(files[i])) {
-        imagePreview(files[i]);
-        setSelectFile((prevArray) => [...prevArray, files[i]]);
-        console.log(selectedFiles, imagePreviewUrl);
-      } else {
-        alert("파일 형식이 올바르지 않습니다.");
-      }
+  const processImage = (file) => {
+    let reader = new FileReader();
+    reader.onloadend = () => {
+      file["invalid"] = false; // add a new property called invalid
+      file["data"] = reader.result;
+      setPreview([...preview, file]);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleFile = (file) => {
+    if (validateFile(file)) {
+      processImage(file);
+      setSelectedFiles([...selectedFiles, file]);
+    } else {
+      alert("파일 형식이 올바르지 않습니다.");
     }
   };
+
+  const renderPreviewImages = () => {
+    if (preview.length > 0) {
+      return (
+        <PreviewContainer>
+          {preview.map((file, i) => (
+            <PreviewImage key={i}>
+              <XButton onClick={() => removeFile(file)}>
+                <Close
+                  style={{ width: "1rem", height: "1rem" }}
+                  fill="#232735"
+                ></Close>
+              </XButton>
+              <Img
+                src={file.data}
+                style={{ width: "5rem", height: "5rem", marginRight: "1rem" }}
+              />
+            </PreviewImage>
+          ))}
+        </PreviewContainer>
+      );
+    }
+  };
+
   return (
     <Container>
       <DropContainer
@@ -75,69 +102,22 @@ export default function Dropzone() {
         onDragLeave={dragLeave}
         onDrop={fileDrop}
       >
-        <FileDilsplayContainer>
-          <UploadBox>
-            <Upload
-              style={{
-                width: "2rem",
-                height: "2rem",
-                position: "absolute",
-              }}
-            ></Upload>
-            <DropMessage>
-              <Text level={3} color="#8b90a0">
-                파일을 드래그해 놓아주세요.
-              </Text>
-            </DropMessage>
-          </UploadBox>
-        </FileDilsplayContainer>
+        <Upload
+          style={{
+            width: "2rem",
+            height: "2rem",
+            marginBottom: "0.5rem",
+          }}
+        ></Upload>
+        <Text level={3} color="#8b90a0">
+          파일을 드래그해 놓아주세요.
+        </Text>
       </DropContainer>
-      <FileStatusBar>
-        {selectedFiles.map((file, i) => (
-          <PreviewImage key={i}>
-            <XButton onClick={removeFile}>
-              <Close
-                style={{ width: "1rem", height: "1rem" }}
-                fill="#232735"
-              ></Close>
-            </XButton>
-
-            {file ? (
-              <img
-                src={file.data}
-                style={{ width: "5rem", height: "5rem", marginRight: "1rem" }}
-              />
-            ) : null}
-          </PreviewImage>
-        ))}
-      </FileStatusBar>
+      {renderPreviewImages()}
     </Container>
   );
 }
-const DropMessage = styled.div`
-  margin: 4rem 0 0 0;
-  text-align: center;
-`;
-const XButton = styled.button`
-  position: absolute;
-  width: fit-content;
-  height: fit-content;
-  border: none;
-  background: transparent;
-  top: -0.3rem;
-  right: 0.2rem;
-`;
-const UploadBox = styled.div`
-  width: fit-content;
-  height: 5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  flex-direction: column;
-  border: none;
-  background: transparent;
-`;
+
 const Container = styled.div`
   text-align: center;
   justify-content: center;
@@ -146,23 +126,19 @@ const Container = styled.div`
   height: fit-content;
   margin: 1.5rem 0 0 0;
 `;
-const FileDilsplayContainer = styled.div`
-  width: 100%;
-  height: 10rem;
-  align-items: center;
-  justify-content: center;
+
+const DropContainer = styled.div`
   flex-direction: column;
   display: flex;
-`;
-const PreviewImage = styled.div`
-  position: relative;
-  width: fit-content;
-  margin-top: 0.8rem;
-  height: fit-content;
   align-items: center;
-  display: flex;
+  justify-content: center;
+  border: 0.1rem dashed #c8acee;
+  width: 100%;
+  height: 10rem;
+  box-sizing: border-box;
 `;
-const FileStatusBar = styled.div`
+
+const PreviewContainer = styled.div`
   position: relative;
   width: 100%;
   height: 6rem;
@@ -172,12 +148,25 @@ const FileStatusBar = styled.div`
   overflow-x: scroll;
 `;
 
-const DropContainer = styled.div`
-  flex-direction: row;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 0.1rem dashed #c8acee;
-  width: 100%;
-  height: 10rem;
+const XButton = styled.button`
+  position: absolute;
+  width: 1rem;
+  height: 1rem;
+  border: none;
+  background: transparent;
+  top: -0.3rem;
+  right: 0.65rem;
+  box-sizing: border-box;
+  padding: unset;
 `;
+
+const PreviewImage = styled.div`
+  position: relative;
+  width: fit-content;
+  margin-top: 0.8rem;
+  height: fit-content;
+  align-items: center;
+  display: flex;
+`;
+
+const Img = styled.img``;
