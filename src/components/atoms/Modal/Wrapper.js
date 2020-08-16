c1ff43533512c2a8c63211ba30711aec16ec389d
soeun lee <thdmsdl87@gmail.com>
2020-08-17 02:13:50 +0900

    Fix Indentation

--- src/components/atoms/Modal/Wrapper.js
+++ src/components/atoms/Modal/Wrapper.js
@@ -1,19 +1,19 @@
 import styled, { css } from "styled-components";
 
 export default styled.div`
-	${(props) => css`
-		box-sizing: border-box;
-		display: ${(props) => (props.visible ? "block" : "none")};
-		position: absolute;
-		height: ${props.height};
-		top: 3.5rem;
-		left: 0;
-		bottom: 0;
-		right: 0;
-		z-index: 1000;
-		overflow-y: auto;
-		overflow-x: hidden;
-		max-height: 62rem;
-		outline: 0;
-	`}
+  ${(props) => css`
+    box-sizing: border-box;
+    display: ${(props) => (props.visible ? "block" : "none")};
+    position: absolute;
+    height: ${props.height};
+    top: 3.5rem;
+    left: 15rem;
+    bottom: 0;
+    right: 15rem;
+    z-index: 1000;
+    overflow-x: hidden;
+    overflow-y: auto;
+    outline: 0;
+    max-height: 62rem;
+  `}
 `;
