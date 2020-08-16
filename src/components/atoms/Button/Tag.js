9b2fe3cdfe1a6eac55b375e17f959da1d5079acc
soeun lee <thdmsdl87@gmail.com>
2020-08-16 12:17:03 +0900

    Fix Indentation

--- src/components/atoms/Button/Tag.js
+++ src/components/atoms/Button/Tag.js
@@ -1,15 +1,14 @@
-import styled, {css} from "styled-components";
-
+import styled, { css } from "styled-components";
 export default styled.button`
-${(props) => css`
-  width: fit-content;
-  height: 1rem;
-  border-radius: 0.5rem;
-  background-color: ${props.backgroundColor};
-  justify-content: center;
-  align-items: center;
-  padding: 0 0.6rem 0 0.6rem;
-  border: none;
-  margin: 0 0.1rem 0 0.1rem;
+  ${(props) => css`
+    width: fit-content;
+    height: 1rem;
+    border-radius: 0.5rem;
+    background-color: ${props.backgroundColor};
+    justify-content: center;
+    align-items: center;
+    padding: 0 0.6rem 0 0.6rem;
+    border: none;
+    margin: 0 0.1rem 0 0.1rem;
   `}
 `;
