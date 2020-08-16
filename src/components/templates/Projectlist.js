a9ecb83ed4400fd21380245a4e725f2814bc9791
chorom-ham <chorom.m.ham@gmail.com>
2020-08-16 17:56:34 +0900

    Add getProjectList function

--- src/components/templates/Projectlist.js
+++ src/components/templates/Projectlist.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import ProjectBlock from "../organisms/ProjectBlock";
 
-export default function Projectlist() {
+export default function Projectlist(props) {
+  const { category, field, region, projectType, query, sort } = props;
+  const { data, isLoading } = getProjectList(
+    category,
+    field,
+    region,
+    projectType,
+    query,
+    sort
+  );
+
   return (
     <Wrapper>
       <Div>
@@ -26,6 +38,46 @@ export default function Projectlist() {
     </Wrapper>
   );
 }
+
+const getProjectList = (category, field, region, projectType, query, sort) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState();
+  const sortColumn = {
+    최신순: "createdDate",
+    댓글순: "commentsNum",
+    조회순: "viewNum",
+  };
+
+  //이후 페이지네이션 관련 코드 추가 필요
+  let body = {
+    page: 0,
+    size: 10,
+    sortColumn: sortColumn[sort],
+    category: category,
+    huntingField: field,
+    region: region,
+    projectCategory: category,
+    keyword: query,
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const result = await axios.post(
+          `${process.env.API_HOST}/projects/list`,
+          body
+        );
+        setData(result.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [category, field, region, projectType, query, sort]);
+  return { data, isLoading };
+};
 
 const Wrapper = styled.div`
   width: 100%;
