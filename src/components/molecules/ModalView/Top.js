82341ff7dcf6c908cfc173d5a64f4c7128d1c9b2
soeun lee <thdmsdl87@gmail.com>
2020-08-17 14:03:47 +0900

    Fix margin to arrange profile name and picture

--- src/components/molecules/ModalView/Top.js
+++ src/components/molecules/ModalView/Top.js
@@ -5,45 +5,65 @@ import Profile from "../Profile";
 import Filterinfo from "../FilterInfo";
 
 export default function ModalTop(props) {
-	return (
-		<Top hight="4rem">
-			<Div>
-				<TitleFilterWrapper>
-					<TitleWrapper>
-						<Text level={12} color="#9c69e2" weight={800}>
-							Title
-						</Text>
-					</TitleWrapper>
-
-					<Filterinfo type={props.type}></Filterinfo>
-				</TitleFilterWrapper>
-				<Profile direction="column" size="2rem" level={2} name="name" weight={800}></Profile>
-			</Div>
-		</Top>
-	);
+  return (
+    <Top hight="4rem">
+      <Div>
+        <TitleFilterWrapper>
+          <TitleWrapper>
+            <Text level={12} color="#9c69e2" weight={800}>
+              Title
+            </Text>
+          </TitleWrapper>
+          <Filterinfo type={props.type}></Filterinfo>
+        </TitleFilterWrapper>
+        <ProfileBox>
+          <Profile direction="column" size="2rem"></Profile>
+          <TextDiv>
+            <Text level={3} weight={800}>
+              name
+            </Text>
+          </TextDiv>
+        </ProfileBox>
+      </Div>
+    </Top>
+  );
 }
 
 const Div = styled.div`
-	width: 100%;
-	height: 100%;
-	display: flex;
-	align-items: center;
-	flex-direction: row;
-	justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const TextDiv = styled.div`
+  width: 2rem;
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  margin: 0 0.3rem 0 0;
 `;
 const TitleWrapper = styled.div`
-	width: 30rem;
-	height: fit-content;
-	display: flex;
-	align-items: left;
-	margin: 0 0 0.5rem 0rem;
+  width: 30rem;
+  height: fit-content;
+  display: flex;
+  align-items: left;
+  margin: 0 0 0.5rem 0rem;
 `;
 
 const TitleFilterWrapper = styled.div`
-	width: 100%;
-	height: 3rem;
-	display: flex;
-	align-items: left;
-	margin: 0 0 1rem 0rem;
-	flex-direction: column;
+  width: 100%;
+  height: 3rem;
+  display: flex;
+  align-items: left;
+  margin: 0 0 1rem 0rem;
+  flex-direction: column;
+`;
+const ProfileBox = styled.div`
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
