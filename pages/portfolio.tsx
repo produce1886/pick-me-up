7cfcace541972d0583f23e7ceec3f6b10e8500a5
chorom-ham <chorom.m.ham@gmail.com>
2020-08-18 21:11:14 +0900

    Change .js file to .tsx file

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,15 +1,17 @@
-import styled from "styled-components";
 import Gnb from "../src/components/organisms/Gnb";
 import FilterSearch from "../src/components/organisms/FilterSearch";
 import BottomButtons from "../src/components/organisms/BottomButtons";
 import PortfolioBody from "../src/components/templates/PortfolioBody";
+import Footer from "../src/components/organisms/Footer";
+
 export default function Portfolio() {
-	return (
-		<>
-			<Gnb></Gnb>
-			<FilterSearch type="portfolio"></FilterSearch>
-			<PortfolioBody></PortfolioBody>
-			<BottomButtons background="#f5edff"></BottomButtons>
-		</>
-	);
+  return (
+    <>
+      <Gnb></Gnb>
+      <FilterSearch type="portfolio"></FilterSearch>
+      <PortfolioBody></PortfolioBody>
+      <BottomButtons></BottomButtons>
+      <Footer></Footer>
+    </>
+  );
 }
