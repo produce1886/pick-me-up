dd36acac1536cc0b353225141f70b0ab7f741511
soeun lee <thdmsdl87@gmail.com>
2020-08-20 19:54:27 +0900

    Add Post request for writing project

--- src/components/molecules/Modal/ModalBottom.js
+++ src/components/molecules/Modal/ModalBottom.js
@@ -6,7 +6,6 @@ import WriteBlock from "../Button/WriteBlock";
 import { useState } from "react";
 import axios from "axios";
 export default function ModalBottom(props) {
-  const { project } = getProject();
   let newtaglists = [];
   const [taginput, setTagInput] = useState("");
   const [tagArray, setTagArray] = useState([]);
@@ -60,7 +59,7 @@ export default function ModalBottom(props) {
           </TagWrapper>
           <ButtonWrapper
             onClick={() => {
-              Write;
+              Write();
               props.onClose();
             }}
           >
@@ -71,29 +70,41 @@ export default function ModalBottom(props) {
     </>
   );
 }
-const getProject = () => {
-  const [project, setProject] = useState();
-  const Write = async () => {
-    try {
-      const result = await axios.post(`${process.env.API_HOST}/projects`, {
+const Write = async (props) => {
+  try {
+    const result = await axios.post(
+      `${process.env.API_HOST}/projects`,
+      {
         title: props.title,
         content: props.content,
         email: props.email,
         category: props.category,
         huntingField: props.field,
         region: props.region,
-        projectCategory: props.projectType,
+        projectCategory: props.projectCategory,
         tags: tagArray,
-      });
-      setProject(result.data);
-      console.log(result.data);
-    } catch (error) {
-      console.log(error);
-    } finally {
+      },
+      { headers: { "Content-Type": "application/json" } }
+    );
+    alert(props);
+  } catch (error) {
+    if (error.response) {
+      // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // 요청이 이루어 졌으나 응답을 받지 못했습니다.
+      // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+      // Node.js의 http.ClientRequest 인스턴스입니다.
+      console.log(error.request);
+    } else {
+      // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
+      console.log("Error", error.message);
     }
-  };
-  Write();
-  return { project };
+    console.log(error.config);
+  } finally {
+  }
 };
 
 const ButtonWrapper = styled.div`
