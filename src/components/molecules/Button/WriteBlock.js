7ecd637699564bd375c99f542d42f065e174680c
chorom-ham <chorom.m.ham@gmail.com>
2020-08-25 21:19:34 +0900

    Refactor and fix codes about writing a post

--- src/components/molecules/Button/WriteBlock.js
+++ src/components/molecules/Button/WriteBlock.js
@@ -1,19 +1,13 @@
 import styled from "styled-components";
-import Link from "next/link";
 import Text from "../../atoms/Text";
 import Wrapper from "../../atoms/Button/Pill";
-export default function MoreButton(props) {
-	return (
-		<Link href={props.link}>
-			<A>
-				<Wrapper>
-					<Text level={1} weight={500} color="#ffffff">
-						글 작성하기
-					</Text>
-				</Wrapper>
-			</A>
-		</Link>
-	);
-}
 
-const A = styled.a``;
+export default function MoreButton(props) {
+  return (
+    <Wrapper>
+      <Text level={1} weight={500} color="#ffffff">
+        글 작성하기
+      </Text>
+    </Wrapper>
+  );
+}
