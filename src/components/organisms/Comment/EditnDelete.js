499d02e828ceaab2045b4839c215cdbceb2b9809
soeun lee <thdmsdl87@gmail.com>
2020-08-30 02:17:30 +0900

    Add modal closing when clicked edit/delete, Add delete portfolio comment

--- src/components/organisms/Comment/EditnDelete.js
+++ src/components/organisms/Comment/EditnDelete.js
@@ -1,12 +1,10 @@
 import ItemWrapper from "../../molecules/Filter/FilterItem";
 import Wrapper from "../../atoms/Filter/DropDownMenu";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { useState } from "react";
 import axios from "axios";
 
 export default function EditnDelete(props) {
   const pid = parseInt(props.pid, 10);
-  const router = useRouter();
   const data = [
     { key: 0, title: "댓글 수정", type: "more", mode: "edit" },
     { key: 1, title: "댓글 삭제", type: "more", mode: "delete" },
@@ -16,11 +14,22 @@ export default function EditnDelete(props) {
   const deleteComment = () => {
     const fetchData = async () => {
       try {
-        if (window.confirm("댓글을 삭제하시겠습니까?")) {
-          const result = await axios.delete(
-            `${process.env.API_HOST}/projects/${pid}/comments/${props.id}`
-          );
-          setComment(result.data);
+        if (props.type === "project") {
+          if (window.confirm("댓글을 삭제하시겠습니까?")) {
+            const result = await axios.delete(
+              `${process.env.API_HOST}/projects/${pid}/comments/${props.id}`
+            );
+            setComment(result.data);
+            props.setClicked(false);
+          }
+        } else if (props.type === "portfolio") {
+          if (window.confirm("댓글을 삭제하시겠습니까?")) {
+            const result = await axios.delete(
+              `${process.env.API_HOST}/portfolios/${pid}/comments/${props.id}`
+            );
+            setComment(result.data);
+            props.setClicked(false);
+          }
         }
       } catch (error) {
         console.log(error);
@@ -34,12 +43,21 @@ export default function EditnDelete(props) {
   const getComment = () => {
     const fetchData = async () => {
       try {
-        const result = await axios.get(
-          `${process.env.API_HOST}/projects/${pid}/comments/${props.id}`
-        );
-        const comment = Object.assign(result.data);
-        props.setCommentUpdate(comment);
-        props.setContentUpdate(comment.content);
+        if (props.type === "project") {
+          const result = await axios.get(
+            `${process.env.API_HOST}/projects/${pid}/comments/${props.id}`
+          );
+          const comment = Object.assign(result.data);
+          props.setCidUpdate(comment.id);
+          props.setContentUpdate(comment.content);
+        } else if (props.type === "portfolio") {
+          const result = await axios.get(
+            `${process.env.API_HOST}/portfolios/${pid}/comments/${props.id}`
+          );
+          const comment = Object.assign(result.data);
+          props.setCidUpdate(comment.id);
+          props.setContentUpdate(comment.content);
+        }
       } catch (error) {
         console.log(error);
       } finally {
@@ -51,6 +69,7 @@ export default function EditnDelete(props) {
   const UpdateComment = () => {
     getComment();
     props.setEdit(true);
+    props.setClicked(false);
   };
   const setSelected = (item) => {
     if (item.key === 1) {
