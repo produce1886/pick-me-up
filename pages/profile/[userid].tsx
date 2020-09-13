80fa1a5a858724190cdb691e59f785113f5a59ce
chorom-ham <chorom.m.ham@gmail.com>
2020-09-13 19:00:40 +0900

    Add Profile page

---
+++ pages/profile/[userid].tsx
@@ -0,0 +1,11 @@
+import Gnb from "../../src/components/organisms/Gnb";
+import Footer from "../../src/components/organisms/Footer";
+
+export default function Profile() {
+  return (
+    <>
+      <Gnb></Gnb>
+      <Footer></Footer>
+    </>
+  );
+}
