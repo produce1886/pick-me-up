ad9a87a4925b2fedb89100d0ae67194ca9dea707
chorom-ham <chorom.m.ham@gmail.com>
2020-09-13 20:17:18 +0900

    Add tab buttons in profile page

--- src/components/templates/ProfileBody.js
+++ src/components/templates/ProfileBody.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import Top from "../organisms/Profile/Top";
+import Tab from "../organisms/Profile/Tab";
 
 function ProfileBody() {
+  const [selected, setSelected] = useState(0);
+
   return (
     <>
       <Top></Top>
+      <Tab selected={selected} setSelected={setSelected}></Tab>
     </>
   );
 }
