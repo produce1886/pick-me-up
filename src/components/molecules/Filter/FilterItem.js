e054851ff748f7b5fe9204890c9847137c69938a
chorom-ham <chorom.m.ham@gmail.com>
2020-10-10 20:51:57 +0900

    Refactor atoms/Filter/* with typescript

--- src/components/molecules/Filter/FilterItem.js
+++ src/components/molecules/Filter/FilterItem.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Wrapper from "../../atoms/Filter/FilterItem";
+import Wrapper from "../../atoms/Filter/Item";
 import Text from "../../atoms/Text";
 
 function FilterItem(props) {
