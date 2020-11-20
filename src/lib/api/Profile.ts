b2586ca95292105a58077f5cf13aaf679fa590a4
chorom-ham <chorom.m.ham@gmail.com>
2020-11-21 03:06:37 +0900

    Refactor profile api

---
+++ src/lib/api/Profile.ts
@@ -0,0 +1,23 @@
+import { Profile } from "@src/types/Data";
+import base from "./Api";
+
+const getProfile = (uid: string) => {
+  if (uid) {
+    return base()
+      .get(`/users/${uid}`)
+      .then((res) => res.data);
+  }
+  return null;
+};
+
+const updateProfile = (uid: string, body: Profile) =>
+  base()
+    .put(`/users/${uid}`, body)
+    .catch((err) => alert("프로필 수정에 실패했습니다"));
+
+const ProfileService = {
+  getProfile,
+  updateProfile,
+};
+
+export default ProfileService;
