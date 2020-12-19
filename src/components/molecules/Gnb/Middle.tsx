fd025749058e95f607d104f722b316b01172df84
chorom-ham <chorom.m.ham@gmail.com>
2020-12-19 23:13:14 +0900

    Change semantic tags

--- src/components/molecules/Gnb/Middle.tsx
+++ src/components/molecules/Gnb/Middle.tsx
@@ -7,7 +7,7 @@ export default function Main() {
   return (
     <Wrapper>
       <InnerWrapper>
-        <Button>
+        <LinkButton>
           <Link href="/project">
             <A>
               <Text level={3} weight={500} color={Colors.BLACK}>
@@ -15,8 +15,8 @@ export default function Main() {
               </Text>
             </A>
           </Link>
-        </Button>
-        <Button>
+        </LinkButton>
+        <LinkButton>
           <Link href="/portfolio">
             <A>
               <Text level={3} weight={500} color={Colors.BLACK}>
@@ -24,28 +24,32 @@ export default function Main() {
               </Text>
             </A>
           </Link>
-        </Button>
+        </LinkButton>
       </InnerWrapper>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.nav`
   width: 20rem;
   justify-content: center;
   display: flex;
   flex-direction: row;
 `;
 
-const InnerWrapper = styled.div`
+const InnerWrapper = styled.ul`
   width: 12rem;
   height: 1.5rem;
   justify-content: space-evenly;
   display: flex;
   flex-direction: row;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  align-items: center;
 `;
 
-const Button = styled.button`
+const LinkButton = styled.li`
   border: none;
   background-color: transparent;
 `;
